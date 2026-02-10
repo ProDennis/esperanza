b5a6140bbf2abcde40d8831fc8346d2eb0867eda
ProDennis <dennisbusiness2016@gmail.com>
2026-02-10 12:50:21 -0600

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/esperanza/',
+  base: 'https://prodennis.github.io/esperanza',
   plugins: [
     react(),
     tailwindcss(),
